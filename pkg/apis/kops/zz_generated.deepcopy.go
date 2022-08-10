9a870f1bf7208f9533fce01cd24893b522d4d0b5
ederst <stefan.prietl@bearingpoint.com>
2022-08-10 10:10:58 +0200

    WIP: Add OCCM address sort order option
    This will add the OCCM option to specify an address sort order, which is
    currently not merged:
    * https://github.com/kubernetes/cloud-provider-openstack/pull/1946

--- pkg/apis/kops/zz_generated.deepcopy.go
+++ pkg/apis/kops/zz_generated.deepcopy.go
@@ -5001,6 +5001,11 @@ func (in *OpenstackNetwork) DeepCopyInto(out *OpenstackNetwork) {
 			}
 		}
 	}
+	if in.AddressSortOrder != nil {
+		in, out := &in.AddressSortOrder, &out.AddressSortOrder
+		*out = new(string)
+		**out = **in
+	}
 	return
 }
 
